120b488a154068cba0318f395dd85331e5c43671
sloppylopez <18N3m3s1s>
2015-10-18 13:53:36 +0200

    :new: fix ci-cd, improved way of deploying using new features of firebase-tools

--- deploy.prod.js
+++ deploy.prod.js
@@ -1,14 +1,27 @@
 var client = require('firebase-tools');
 
-client.deploy({
-    firebase: 'sloppy',
-    token: process.env.FIREBASE_TOKEN,
-    cwd: '.'
+console.log(process.env.FIREBASE_TOKEN);
+
+client.login({
+    email: process.env.FIREBASE_USER,
+    password: process.env.FIREBASE_PASSWORD
 }).then(function () {
-    console.log('Angulpar has been deployed in production!');
-    process.exit(0);
+    console.log('Firebase has logged');
+    client.deploy({
+        firebase: 'sloppy',
+        token: process.env.FIREBASE_TOKEN,
+        cwd: '.'
+    }).then(function () {
+        console.log('Angulpar has been deployed in production!');
+        process.exit(0);
+    }).catch(function (err) {
+        // handle error
+        console.log(err);
+        process.exit(1);
+    });
 }).catch(function (err) {
     // handle error
     console.log(err);
     process.exit(1);
 });
+
